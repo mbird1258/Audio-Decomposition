f46564a57e1793ba3ab65db4b888987d278cf36d
mbird1258 <132913097+mbird1258@users.noreply.github.com>
2024-11-13 18:24:40 +0800

    fix indentation

--- ScrapeInstruments.py
+++ ScrapeInstruments.py
@@ -6,63 +6,63 @@ import os
 BaseDir = "https://theremin.music.uiowa.edu/"
 Debug = False
 
-    urls = ["https://theremin.music.uiowa.edu/MISpiano.html",
-            "https://theremin.music.uiowa.edu/MISguitar.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISFlute2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISaltoflute2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBassFlute2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISOboe2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISEbClarinet2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBbClarinet2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBbBassClarinet2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBassoon2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBbSopranoSaxophone2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISEbAltoSaxophone2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISHorn2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBbTrumpet2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISTenorTrombone2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBassTrombone2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISTuba2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISViolin2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISViola2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISCello2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISDoubleBass2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISMarimba2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISxylophone2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISVibraphone2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBells2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISCymbals2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISGongsTamTams2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISHandPercussion2012.html",
-            "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISTambourines2012.html"]
+urls = ["https://theremin.music.uiowa.edu/MISpiano.html",
+        "https://theremin.music.uiowa.edu/MISguitar.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISFlute2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISaltoflute2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBassFlute2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISOboe2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISEbClarinet2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBbClarinet2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBbBassClarinet2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBassoon2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBbSopranoSaxophone2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISEbAltoSaxophone2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISHorn2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBbTrumpet2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISTenorTrombone2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBassTrombone2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISTuba2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISViolin2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISViola2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISCello2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISDoubleBass2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISMarimba2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISxylophone2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISVibraphone2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBells2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISCymbals2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISGongsTamTams2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISHandPercussion2012.html",
+        "https://theremin.music.uiowa.edu/MIS-Pitches-2012/MISTambourines2012.html"]
 
-    for url in urls:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
+for url in urls:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-        for element in soup.find_all('p')[2:]:
-            for child in element.findChildren("a", href = True):
-                try:
-                    if child['href'] == "oops.html":
+    for element in soup.find_all('p')[2:]:
+        for child in element.findChildren("a", href = True):
+            try:
+                if child['href'] == "oops.html":
+                    continue
+
+                name = child.text.split(" ")[0].replace('\r', '').replace('\n', '').strip()
+                name = os.path.join(os.path.dirname(os.path.realpath(__file__)), "InstrumentAudioFiles", name)
+
+                if name[-3:] == "zip":
+                    continue
+
+                if os.path.isfile(name):
+                    if os.path.getsize(name):
+                        if Debug: print(f"{name} already exists, skipping")
                         continue
+                elif os.path.isdir(name):
+                    continue
 
-                    name = child.text.split(" ")[0].replace('\r', '').replace('\n', '').strip()
-                    name = os.path.join(os.path.dirname(os.path.realpath(__file__)), "InstrumentAudioFiles", name)
+                with open(name, 'wb') as file:
+                    file.write(urllib.request.urlopen(urllib.parse.urljoin(BaseDir, child['href'].replace(" ", "%20"))).read())
+                    if Debug: print(f"{name} successfully added")
+            except Exception as e:
+                if Debug: print(f"\n\n\n Exception encountered: {e} \n\n\n Skipped file: {name} \n\n\n")
 
-                    if name[-3:] == "zip":
-                        continue
-
-                    if os.path.isfile(name):
-                        if os.path.getsize(name):
-                            if Debug: print(f"{name} already exists, skipping")
-                            continue
-                    elif os.path.isdir(name):
-                        continue
-
-                    with open(name, 'wb') as file:
-                        file.write(urllib.request.urlopen(urllib.parse.urljoin(BaseDir, child['href'].replace(" ", "%20"))).read())
-                        if Debug: print(f"{name} successfully added")
-                except Exception as e:
-                    if Debug: print(f"\n\n\n Exception encountered: {e} \n\n\n Skipped file: {name} \n\n\n")
-
-    print("\nfin")
+print("\nfin")
